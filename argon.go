bbb2dabc243351fd6f75ff00f63536a5dfd5ae2a
kamalshkeir <kamalshkeir@yahoo.fr>
2023-01-03 23:23:56 +0100

    argon2

---
+++ argon.go
@@ -0,0 +1,68 @@
+package argon
+
+import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
+)
+
+type passwordConfig struct {
+	time    uint32
+	memory  uint32
+	threads uint8
+	keyLen  uint32
+}
+
+func Hash(password string) (string, error) {
+	c := &passwordConfig{
+		time:    1,
+		memory:  64 * 1024,
+		threads: 4,
+		keyLen:  32,
+	}
+	// Generate a Salt
+	salt := make([]byte, 16)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+
+	hash := argon2.IDKey([]byte(password), salt, c.time, c.memory, c.threads, c.keyLen)
+
+	// Base64 encode the salt and hashed password.
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	format := "$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s"
+	full := fmt.Sprintf(format, argon2.Version, c.memory, c.time, c.threads, b64Salt, b64Hash)
+	return full, nil
+}
+
+func Match(hash, clearIn string) bool {
+
+	parts := strings.Split(hash, "$")
+
+	c := &passwordConfig{}
+	_, err := fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d", &c.memory, &c.time, &c.threads)
+	if err != nil {
+		return false
+	}
+
+	salt, err := base64.RawStdEncoding.DecodeString(parts[4])
+	if err != nil {
+		return false
+	}
+
+	decodedHash, err := base64.RawStdEncoding.DecodeString(parts[5])
+	if err != nil {
+		return false
+	}
+	c.keyLen = uint32(len(decodedHash))
+
+	comparisonHash := argon2.IDKey([]byte(clearIn), salt, c.time, c.memory, c.threads, c.keyLen)
+
+	return (subtle.ConstantTimeCompare(decodedHash, comparisonHash) == 1)
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/kamalshkeir/argon
+
+go 1.19
+
+require golang.org/x/crypto v0.4.0
+
+require golang.org/x/sys v0.3.0 // indirect
